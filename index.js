7e79adc31306813f6e618e31d4dc735ec560530b
giegeralexandra <giegeralexandra@gmail.com>
2021-02-27 16:52:26 -0500

    commit tree ac8ffa6454e23dffc1cd9f6d7fb7e513840f2f73

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newArray = tutorials.map(function(tutorial){
+    return tutorial.split(" ").map((word) =>{
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ")
+  })
+  return newArray;
 }
+
+
